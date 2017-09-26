4b4c689cb5a4624a1c606bafd9d3780f998fe8ce
atulsingla82 <asingla@wgu.edu>
2017-09-26 17:01:40 -0400

    Added express-handlebars

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 //require express
 const express = require('express');
-
+const exphbs  = require('express-handlebars');
 const app = express();
+
+//Index route
+app.get('/', (req, res) => {
+  
+  res.render('index')
+
+});
+
+//About page
+
+app.get('/about',(req,res) => {
+
+	res.send('about')
+})
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 //setting port
 const port = 5000;
