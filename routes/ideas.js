1d5528f84663516ad92e185bf635d65f849c0b94
atulsingla82 <asingla@wgu.edu>
2017-10-05 22:45:28 -0400

    Ready for deployment!

--- routes/ideas.js
+++ routes/ideas.js
@@ -20,11 +20,16 @@ router.get('/edit/:id',ensureAuthenticated,(req,res) => {
 	Idea.findOne({
 		_id:req.params.id
 	})
-	.then(idea =>{
-res.render('ideas/edit',{
+	.then(idea => {
+    if(idea.user !== req.user.id){
+      req.flash('error_msg', 'Not Authorized');
+      res.redirect('/ideas');
+    } else{
+     res.render('ideas/edit',{
          
          idea:idea
        });
+    }
 
 	});
 
@@ -32,7 +37,7 @@ res.render('ideas/edit',{
 
 //Add ideas index page route
 router.get('/',ensureAuthenticated,(req,res) => {
-	Idea.find({})
+	Idea.find({user:req.user.id})
 	.sort({date:'desc'})
 	.then(ideas => {
 		res.render('ideas/index',{
@@ -65,7 +70,7 @@ router.post('/',ensureAuthenticated, (req, res) =>{
   } else {	
   	const newUser = {
        title:req.body.title,
-       details:req.body.details
+       details:req.body.details,
        user:req.user.id
   	}
 
